59fdab02b61f795dbb0dcfec5097732ca21d06a5
BingXuanOwO <hxc35459@outlook.com>
2024-10-23 01:13:48 +0800

    lab1

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,8 +1,8 @@
 //! Process management syscalls
 use crate::{
     config::MAX_SYSCALL_NUM,
-    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus},
-    timer::get_time_us,
+    task::{exit_current_and_run_next, get_current_task, suspend_current_and_run_next, TaskStatus},
+    timer::{get_time_ms, get_time_us},
 };
 
 #[repr(C)]
@@ -53,5 +53,11 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
-    -1
+    let task = get_current_task();
+    unsafe{
+        (*_ti).status = task.task_status;
+        (*_ti).syscall_times = task.syscall_times;
+        (*_ti).time = get_time_ms() - task.exec_time;
+    }
+    0
 }
